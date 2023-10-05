03fbe2ddc3f2050c2e7263707f5f5e110b75a3a3
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-10-05 13:54:19 +0100

    almost done

--- src/components/TRAININGS/SecThree.js
+++ src/components/TRAININGS/SecThree.js
@@ -8,7 +8,7 @@ export default function SecThree() {
         <h2 className="font-bold text-4xl text-center my-10 overflow-visible">Upcoming Events</h2>
         <div className="flex flex-col py-5 px-4">
           {Events.map((event, index) => (
-            <div className='flex border flex-col md:flex-row relative gap-4 md:gap-20 overflow-visible py-10' key={index}>
+            <div className='flex border px-2 flex-col md:flex-row relative gap-4 md:gap-20 overflow-visible py-10' key={index}>
               <div className='relative overflow-visible h-auto md:w-1/4'>
                 <span className='font-bold text-3xl'>{event.day}</span>
                 <span className='absolute font-semibold -top-2'>{event.month}</span>
