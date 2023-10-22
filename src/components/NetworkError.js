bf88bf86e842e11e249099b8c592e10ebc7d1cc6
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-10-22 20:57:55 +0100

    small changes

--- src/components/NetworkError.js
+++ src/components/NetworkError.js
@@ -3,8 +3,24 @@ import React from 'react';
 const NetworkError = () => {
   return (
     <div className="bg-red-500 min-h-screen flex flex-col items-center justify-center text-white">
-      <h1 className="text-3xl font-semibold mb-4">Network Error</h1>
-      <p className="text-lg">Please check your internet connection and try again.</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-16 h-16 text-red-800 mb-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+      <h1 className="text-4xl font-extrabold mb-2">Network Error</h1>
+      <p className="text-lg">
+        It seems there's an issue with your network. Please check your internet connection and try again.
+      </p>
     </div>
   );
 };
